485407bd4b669434485683ddfae4e0c9616eb3fe
IreneuszKopiec <115220213+IreneuszKopiec@users.noreply.github.com>
2023-11-07 23:23:31 +0100

    insert sort

---
+++ insertSort/pd/pd/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int[] arr = covert(textBox1.Text);
+            int[] arr2 = insertSort(arr);
+            string wynik = toString(arr2);
+            label1.Text = wynik;
+        }
+
+        static int[] insertSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        public static string toString(int[] arr)
+        {
+            string wynik = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                wynik += arr[i] + " ";
+            }
+            return wynik;
+        }
+
+        public static int[] covert(string text)
+        {
+            var liczbyS = text.Trim().Split(' ');
+            int[] liczby = new int[liczbyS.Length];
+            for (int i = 0; i < liczbyS.Length; i++)
+            {
+                liczby[i] = int.Parse(liczbyS[i]);
+            }
+            return liczby;
+        }
+
+    }
+}
