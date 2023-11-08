9616087e36e29bab4a9ecfd56a8b8b4883e9aaba
IreneuszKopiec <115220213+IreneuszKopiec@users.noreply.github.com>
2023-11-08 11:13:41 +0100

    Add files via upload

---
+++ ISIgr1_5/ISIgr1_5/Form1.cs
@@ -0,0 +1,58 @@
+namespace ISIgr1_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var w1 = new Wezel(5);
+            
+
+            var w2 = new Wezel(3);
+            var w3 = new Wezel(1);
+            var w4 = new Wezel(2);
+            var w5 = new Wezel(6);
+            var w6 = new Wezel(4);
+            w1.dzieci.Add(w2);
+            w1.dzieci.Add(w3);
+            w1.dzieci.Add(w4);
+            w2.dzieci.Add(w5);
+            w2.dzieci.Add(w6);
+
+            A(w1);
+
+
+        }
+        void A(Wezel w)
+        {
+            MessageBox.Show(w.wartosc.ToString());
+            //A(w);
+
+            for(int i=0;i<w.dzieci.Count;i++)
+            {
+                A(w.dzieci[i]);
+            }
+        }
+    }
+
+    class Wezel
+    {
+        public int wartosc;
+        public List<Wezel> dzieci = new List<Wezel>();
+
+        public Wezel(int liczba)
+        {
+            this.wartosc = liczba;
+        }
+    }
+
+}
