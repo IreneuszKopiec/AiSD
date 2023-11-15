166027a6975169a31cf8be66d765b385f1a17fb3
IreneuszKopiec <115220213+IreneuszKopiec@users.noreply.github.com>
2023-11-15 11:11:31 +0100

    Add files via upload

--- ISIgr1_5/ISIgr1_5/Form1.cs
+++ ISIgr1_5/ISIgr1_5/Form1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace ISIgr1_5
 {
     public partial class Form1 : Form
@@ -14,9 +16,8 @@ namespace ISIgr1_5
 
         private void button1_Click(object sender, EventArgs e)
         {
+            /*
             var w1 = new Wezel(5);
-            
-
             var w2 = new Wezel(3);
             var w3 = new Wezel(1);
             var w4 = new Wezel(2);
@@ -29,15 +30,44 @@ namespace ISIgr1_5
             w2.dzieci.Add(w6);
 
             A(w1);
+            */
+            var w1 = new Wezel2(1);
+            var w2 = new Wezel2(2);
+            var w3 = new Wezel2(3);
+            var w4 = new Wezel2(4);
+            var w5 = new Wezel2(5);
+            var w6 = new Wezel2(6);
+            w1.dzieci.Add(w2);
+            w2.dzieci.Add(w5);
+            w2.dzieci.Add(w4);
+            w4.dzieci.Add(w6);
+            w6.dzieci.Add(w3);
+            w3.dzieci.Add(w1);
+
+            A(w1);
+
+
+
 
 
         }
+        
         void A(Wezel w)
         {
             MessageBox.Show(w.wartosc.ToString());
             //A(w);
 
             for(int i=0;i<w.dzieci.Count;i++)
+            {
+                A(w.dzieci[i]);
+            }
+        }
+        void A(Wezel2 w)
+        {
+            MessageBox.Show(w.wartosc.ToString());
+            //A(w);
+
+            for (int i = 0; i < w.dzieci.Count; i++)
             {
                 A(w.dzieci[i]);
             }
@@ -52,6 +82,38 @@ namespace ISIgr1_5
         public Wezel(int liczba)
         {
             this.wartosc = liczba;
+        }
+    }
+
+    class Wezel2
+    {
+        public int wartosc;
+        public List<Wezel2> dzieci = new List<Wezel2>();
+
+        public Wezel2(int liczba)
+        {
+            this.wartosc = liczba;
+            
+        }
+
+        public void Add(Wezel2 s)
+        {
+            
+            if(this==s)
+            {
+                return;
+            }
+
+            if(!this.dzieci.Contains(s))
+            {
+                this.dzieci.Add(s);
+               
+            }
+            if(!s.dzieci.Contains(this))
+            {
+                s.dzieci.Add(this);
+                
+            }
         }
     }
 
